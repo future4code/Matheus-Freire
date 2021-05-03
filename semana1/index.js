56a616803964daf1271ec2aa4f341ae055dccbe6
matheuspimentel1795 <matheuspimentel1795@gmail.com>
2021-05-03 14:48:37 -0300

    exercicios aula 3

---
+++ semana1/index.js
@@ -0,0 +1,77 @@
+
+/*
+** Exercícios de interpretação de código**
+1 - 
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+Resposta: Será impresso no console os números 10,5
+
+2 -
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+Resposta - Será impresso no console: 10,10,10
+
+3 - 
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+Resposta: vão haver duas perguntas em sequência na tela, a primeira é quantas horas você trabalha por dia e a outra Quanto você recebe por dia. Após digitar as idades vai aparecer em forma de alerta na tela o resultado de quanto recebe por dia por quantidade de horas trabalhadas 
+Sugestão de nomes para: p - horasTrabalhadasDia
+                        t - recebePorDia
+*/
+
+/*
+let nome 
+let idade
+console.log(typeof nome,typeof idade)
+
+/*
+como não teve um valor então vai retornar undefined na tela
+
+*/
+/*
+const nomeDoUsuario = prompt("Qual seu nome?")
+let idadeDoUsuario = prompt("qual sua idade?")
+console.log("O tipo de variável no nome é",typeof nomeDoUsuario)
+console.log("O tipo de variável na idade é",typeof idadeDoUsuario)
+/*
+Ambas as variáveis que vão aparecer na tela são string
+*/
+/*
+console.log("OLÁ",nomeDoUsuario,"você tem",idadeDoUsuario)
+*/
+/*
+const roupa = prompt("você está usando roupa azul hoje?")
+const time = prompt("você torce para o cruzeiro?")
+const programacao = prompt("você entede de programação?")
+console.log("você está usando roupa azul hoje?",roupa)
+console.log("você torce para o cruzeiro?",time)
+console.log("você entende de programaçao?",programacao)
+*/
+
+
+**exercicio 3**
+
+let a = 10
+let b = 25 
+let c
+c=b
+b=a
+console.log("a=",c)
+console.log("b=",b)
+
+
