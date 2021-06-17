50fdcffe9cd29dec1c139d232259b1928bd88abc
matheuspimentel1795 <matheuspimentel1795@gmail.com>
2021-06-17 16:01:26 -0300

    aula27

---
+++ semana7/spacex/src/components/Nomes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class NomeS extends React.Component{
+    
+    render(){
+        return(
+            <div>{this.props.id}</div>
+        )
+    }
+}
