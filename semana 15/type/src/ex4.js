56799a6653665ff013b8168e31ba5f2ff42df205
matheuspimentel1795 <matheuspimentel1795@gmail.com>
2021-08-10 16:42:55 -0300

    aula typescript

---
+++ semana 15/type/src/ex4.js
@@ -0,0 +1,17 @@
+var pokemon1 = {
+    name: "Charmander",
+    types: "Fire",
+    healthPoints: 28
+};
+var pokemon2 = {
+    name: "Bulbasaur",
+    types: "Grass/Poison",
+    healthPoints: 31
+};
+var pokemon3 = {
+    name: "Squirtle",
+    types: "Water",
+    healthPoints: 35
+};
+// a)  executando o comando: tsc ./build/ex4.ts
+// b) 
---
+++ semana 15/type/src/ex5.js
@@ -0,0 +1,5 @@
+function somar(num1, num2) {
+    var soma = num1 + num2;
+    console.log(soma);
+}
+somar(2, 3);
